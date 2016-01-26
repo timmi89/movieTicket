1bd39252c8d821ea00c19eb924491b9f0c24502e
chris berry and timothea mcguire <chrisberry321@yahoo.com>
2016-01-26 11:36:28 -0800

    working address book, and understanding of children

--- spec/specs.js
+++ spec/specs.js
@@ -13,3 +13,27 @@ describe('Contact', function() {
   });
 
 });
+
+describe('Address', function() {
+  it("creates a new address with the given specifications", function() {
+    var testAddress = new Address("123 Main St", "Test City", "Test State");
+    expect(testAddress.street).to.equal("123 Main St");
+    expect(testAddress.city).to.equal("Test City");
+    expect(testAddress.state).to.equal("Test State");
+  });
+
+
+  it("adds the full address[street, city, state] to an address", function() {
+    var testAddress =  new Address("123 Main St", "Test City", "Test State");
+    expect(testAddress.fullAddress()).to.equal("123 Main St, Test City, Test State");
+  });
+});
+
+
+// describe('Address', function() {
+//   it("creates a new address with the given specifications", function() {
+//     var testAddress = new Address("123 Main St", "Test City","Test State");
+//     expect(testAddress.street).to.equal("123 Main St");
+//     expect(testAddress.city).to.equal("Test City");
+//     expect(testAddress.state).to.equal("Test State");
+//   });
